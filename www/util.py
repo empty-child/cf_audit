5bac1bf530aff7acf46b6878389ccd52c3849a20
Felix Reiniger <felix.reiniger@ost.ch>
2022-12-13 09:17:43 +0100

    open files with EAFP

--- www/util.py
+++ www/util.py
@@ -7,10 +7,16 @@ import time
 
 def read_csv(file):
     if file.exists():
-        with open(file, 'r') as csv_file:
-            return csv_file.read()
+        try:
+            with open(file, 'r') as csv_file:
+                return csv_file.read()
+        except OSError:
+            return f"Couldn't read: {file}"
     else:
-        write_csv_header(file)
+        try:
+            write_csv_header(file)
+        except OSError:
+            return f"Couldn't write: {file}"
     return read_csv(file)
 
 
@@ -25,22 +31,28 @@ def write_csv_header(file):
 def update_csv(file, project, user, ref_id, osm_id, type):
     if not file.exists():
         write_csv_header(file)
+    try:
+        with open(file, 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            exited_before = feature_in_csv(file, ref_id)
+            writer.writerow([project, user, ref_id, osm_id, type, time.time(), exited_before])
 
-    with open(file, 'a') as csv_file:
-        writer = csv.writer(csv_file)
-        exited_before = feature_in_csv(file, ref_id)
-        writer.writerow([project, user, ref_id, osm_id, type, time.time(), exited_before])
-
-        csv_file.close()
+            csv_file.close()
+    except OSError:
+        print(f"Couldn't write: {file}")
 
 
 def feature_in_csv(file, feature_ref):
-    with open(file, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
+    try:
+        with open(file, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
 
-        for row in reader:
-            if row['ref_id'] == feature_ref:
-                return True
+            for row in reader:
+                if row['ref_id'] == feature_ref:
+                    return True
+            return False
+    except OSError:
+        print(f"Couldn't read: {file}")
         return False
 
 
