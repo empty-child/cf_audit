807d420e2a3f6a1a4e553a4ea86f63671e47598d
Felix Reiniger <felix.reiniger@ost.ch>
2023-02-03 13:16:22 +0100

    remove id

--- www/stats_operations.py
+++ www/stats_operations.py
@@ -8,7 +8,12 @@ from flask import abort
 def read_stats():
     csv_output = StringIO()
 
-    writer = csv.DictWriter(csv_output, Stats._meta.fields.keys())
+    keys = Stats._meta.fields.keys();
+
+    new_keys = list(keys)
+    new_keys.remove('id')
+
+    writer = csv.DictWriter(csv_output, new_keys)
     writer.writeheader()
     results = Stats.select().dicts()
 
@@ -18,13 +23,15 @@ def read_stats():
             'timestamp' : result.get('timestamp').isoformat()
                        })
 
+        result.pop('id')
+
     writer.writerows(results)
 
     return csv_output.getvalue()
 
 
 def is_ref_id_in_db(ref_id):
-    return Stats.select().where(Stats.ref_id == ref_id).count() > 0
+    return Stats.select(Stats.project_name).where(Stats.ref_id == ref_id).count() > 0
 
 
 def update_stats(project, user, ref_id, osm_id, osm_type, action):
