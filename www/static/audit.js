792993e3a15fb9d3b5a4669153655c18ddb93332
Felix Reiniger <felix.reiniger@ost.ch>
2023-10-05 09:04:45 +0200

    rewrite tag creation to display them next to each other

--- www/static/audit.js
+++ www/static/audit.js
@@ -535,6 +535,7 @@ function renderTagTable(data, audit, editNewTags) {
     }
   }
 
+
   // Render the table
   function esc(s) {
     s = s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -546,19 +547,22 @@ function renderTagTable(data, audit, editNewTags) {
     return a.length == b.length ? ((a[0] > b[0]) - (b[0] - a[0])) : a.length - b.length;
   });
 
-  var rows = '', notset = '<span class="notset">not set</span>';
-  for (var i = 0; i < keys.length; i++) {
-    key = keys[i];
-    if (key.length == 2)
-      rows += '<tr class="notagedit"><th>' + esc(key[0]) + '</th><td>' + esc(key[1]) + '</td></tr>';
-    else {
-      rows += '<tr class="tagedit"><th rowspan="2">' + esc(key[0]) + '</th>';
-      rows += '<td>' + (!key[1] ? notset : esc(key[1])) + '&nbsp;<input type="radio" name="r'+i+'" value="1-'+i+'"></td>';
-      rows += '</tr><tr class="tagedit lower"><td>' + (!key[2] ? notset : esc(key[2])) + '&nbsp;<input type="radio" name="r'+i+'" value="2-'+i+'"></td></tr>';
+  function buildTable() {
+    var rows = '', notset = '<span class="notset">not set</span>'
+    for(var i = 0; i < key.length; i++){
+      key = keys[i];
+      if (key.length == 2)
+        rows += '<tr class="notagedit"><th>' + esc(key[0]) + '</th><td>' + esc(key[1]) + '</td></tr>'; // CASE NO KEYS?
+      else {
+        rows += '<tr class="tagedit"><th>' + esc(key[0]) + '</th>';
+        rows += '<td>' + (!key[1] ? notset : esc(key[1])) + '&nbsp;<input type="radio" name="r'+i+'" value="1-'+i+'"></td>';
+        rows += '<td>' + (!key[2] ? notset : esc(key[2])) + '&nbsp;<input type="radio" name="r'+i+'" value="2-'+i+'"></td></tr>';
+      }
     }
+    $('#tags').empty().append(rows);
   }
-  $('#tags').empty().append(rows);
 
+  buildTable()
   // Set state of each row
   function cellColor(row, which) {
     if (which == 1)
